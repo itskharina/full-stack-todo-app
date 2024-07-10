81dfa86e11124a7debc7dc07dc82ef7cb2357cb3
itskharina <137315976+itskharina@users.noreply.github.com>
2024-07-10 20:05:38 +0100

    Changed the accordion to include a custom icon and added dropdown menu

--- client/src/routes/Upcoming.tsx
+++ client/src/routes/Upcoming.tsx
@@ -1,10 +1,47 @@
 // import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import React, { forwardRef, useEffect } from 'react';
 import todoService from '../services/todos';
 import { setTodos } from '../store/todoSlice';
 import { useSidebar } from '../components/Sidebar/SidebarProvider';
 import Accordion from 'react-bootstrap/Accordion';
 import { useAppSelector, useAppDispatch } from '../hooks';
+import { useAccordionButton } from 'react-bootstrap/AccordionButton';
+import Card from 'react-bootstrap/Card';
+import { FaInfoCircle } from 'react-icons/fa';
+import { BsThreeDots } from 'react-icons/bs';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+const CustomToggle = ({ eventKey }: { eventKey: string }) => {
+	const decoratedOnClick = useAccordionButton(eventKey, () =>
+		console.log('totally custom!')
+	);
+
+	return (
+		<button type='button' className='info-btn' onClick={decoratedOnClick}>
+			<FaInfoCircle />
+		</button>
+	);
+};
+
+interface CustomDropDownProps {
+	children: React.ReactNode;
+	onClick: (event: React.MouseEvent<HTMLAnchorElement>) => void;
+}
+
+const CustomDropDown = forwardRef<HTMLAnchorElement, CustomDropDownProps>(
+	({ children, onClick }, ref) => (
+		<a
+			href=''
+			ref={ref}
+			onClick={(e) => {
+				e.preventDefault();
+				onClick(e);
+			}}
+		>
+			{children}
+		</a>
+	)
+);
 
 const Upcoming = () => {
 	const { sidebar } = useSidebar();
@@ -21,17 +58,40 @@ const Upcoming = () => {
 			<div className='accordion' style={{ width: '60%', margin: '0 auto' }}>
 				<Accordion>
 					{todos.map((todo, index) => (
-						<Accordion.Item
-							style={{ marginBottom: '20px', borderRadius: '10px' }}
-							eventKey={index.toString()}
-							key={index}
-						>
-							<Accordion.Header>
-								<span>{todo.title}</span>
-							</Accordion.Header>
+						<Card style={{ marginBottom: '20px', borderRadius: '10px' }} key={index}>
+							<Card.Header>
+								<div className='left'>
+									<input type='checkbox' className='checkbox' data-index={index} />
+									<span className='title'>
+										<b>{todo.title}</b>
+									</span>
+								</div>
+								<div className='right'>
+									{todo.dueDate && (
+										<span className='due-date'>
+											<b>Due:</b> {todo.dueDate}
+										</span>
+									)}
+									<span className='priority'>{todo.priority}</span>
+									<CustomToggle eventKey={index.toString()}></CustomToggle>
+									<Dropdown>
+										<Dropdown.Toggle as={CustomDropDown} id='dropdown-custom-component'>
+											<BsThreeDots />
+										</Dropdown.Toggle>
 
-							<Accordion.Body>{todo.todo}</Accordion.Body>
-						</Accordion.Item>
+										<Dropdown.Menu>
+											<Dropdown.Item eventKey='1'>Edit</Dropdown.Item>
+											<Dropdown.Item eventKey='2'>Delete</Dropdown.Item>
+										</Dropdown.Menu>
+									</Dropdown>
+								</div>
+							</Card.Header>
+							<Accordion.Collapse eventKey={index.toString()}>
+								<Card.Body className='description'>
+									<b>Description:</b> {todo.todo}
+								</Card.Body>
+							</Accordion.Collapse>
+						</Card>
 					))}
 				</Accordion>
 			</div>
